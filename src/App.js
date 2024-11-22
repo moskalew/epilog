919237e9d838f55a3d76318025646913c6f288f7
andrei moskalev <moskalev.web.dev@gmail.com>
2024-11-22 04:25:25 +0300

    useCallback()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useState, useCallback } from "react";
 import styles from "./app.module.css";
 
 export const App = () => {
+	const [num, setNum] = useState(0);
+	const [degree, setDegree] = useState(0);
+	const [result, setResult] = useState(0);
+
+	const onNumChange = useCallback(
+		({ target }) => {
+			setNum(Number(target.value));
+			setResult(Math.pow(Number(target.value), degree));
+		},
+		[degree],
+	);
+
+	const onDegreeChange = useCallback(
+		({ target }) => {
+			setDegree(Number(target.value));
+			setResult(Math.pow(num, Number(target.value)));
+		},
+		[num],
+	);
+
 	return (
 		<div className={styles.App}>
-			<div></div>
+			<div>
+				{num} в степени {degree} = {result}
+			</div>
+			<label>
+				<span>Число: </span>
+				<input type="number" value={num} onChange={onNumChange}></input>
+			</label>
+			<label>
+				<span>Степень: </span>
+				<input
+					type="number"
+					value={degree}
+					onChange={onDegreeChange}
+				></input>
+			</label>
 		</div>
 	);
 };
