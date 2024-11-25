6647957453e9f84e361dda74bdcf64f51c9fa537
andrei moskalev <moskalev.web.dev@gmail.com>
2024-11-25 18:33:10 +0300

    4. memo(). Введение в проблему

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import styles from "./app.module.css";
 
+export const Field = ({ name, label, value, onChange }) => {
+	console.log(name);
+	return (
+		<label>
+			<span>{label}: </span>
+			<input
+				type="number"
+				name={name}
+				value={value}
+				onChange={onChange}
+			/>
+		</label>
+	);
+};
+
 export const App = () => {
+	console.log("--------------App---------------");
 	const [num, setNum] = useState(0);
 	const [degree, setDegree] = useState(0);
 	const [result, setResult] = useState(0);
 
-	const onNumChange = useCallback(
-		({ target }) => {
-			setNum(Number(target.value));
-			setResult(Math.pow(Number(target.value), degree));
-		},
-		[degree],
-	);
+	const onNumChange = ({ target }) => {
+		setNum(Number(target.value));
+		setResult(Math.pow(Number(target.value), degree));
+	};
 
-	const onDegreeChange = useCallback(
-		({ target }) => {
-			setDegree(Number(target.value));
-			setResult(Math.pow(num, Number(target.value)));
-		},
-		[num],
-	);
+	const onDegreeChange = ({ target }) => {
+		setDegree(Number(target.value));
+		setResult(Math.pow(num, Number(target.value)));
+	};
 
 	return (
 		<div className={styles.App}>
 			<div>
 				{num} в степени {degree} = {result}
 			</div>
-			<label>
-				<span>Число: </span>
-				<input type="number" value={num} onChange={onNumChange}></input>
-			</label>
-			<label>
-				<span>Степень: </span>
-				<input
-					type="number"
-					value={degree}
-					onChange={onDegreeChange}
-				></input>
-			</label>
+			<Field
+				name="num"
+				label="Число"
+				value={num}
+				onChange={onNumChange}
+			/>
+			<Field
+				name="degree"
+				label="Степень"
+				value={degree}
+				onChange={onDegreeChange}
+			/>
 		</div>
 	);
 };
